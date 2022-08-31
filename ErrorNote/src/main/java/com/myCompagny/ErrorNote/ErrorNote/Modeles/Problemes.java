a58457698c60721cfae13242c46e7f8d6f79645d
jkkoloma <kolomajeremi60@gmail.com>
2022-08-31 17:51:19 +0000

    Dernière version béta de mon API

--- ErrorNote/src/main/java/com/myCompagny/ErrorNote/ErrorNote/Modeles/Problemes.java
+++ ErrorNote/src/main/java/com/myCompagny/ErrorNote/ErrorNote/Modeles/Problemes.java
@@ -1,5 +1,6 @@
 package com.myCompagny.ErrorNote.ErrorNote.Modeles;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
