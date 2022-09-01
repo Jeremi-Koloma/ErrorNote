d8f3ea3f05abc56d6a36c24bd150fd306f827021
jkkoloma <kolomajeremi60@gmail.com>
2022-09-01 10:04:48 +0000

    Dernière modification de notre API

--- ErrorNote/src/main/java/com/myCompagny/ErrorNote/ErrorNote/Repositorys/RepositorySolutions.java
+++ ErrorNote/src/main/java/com/myCompagny/ErrorNote/ErrorNote/Repositorys/RepositorySolutions.java
@@ -5,6 +5,8 @@ import com.myCompagny.ErrorNote.ErrorNote.Modeles.Solutions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 // On étend de l'interface JPA Repository avec <Entity name, Long>
 public interface RepositorySolutions extends JpaRepository<Solutions, Long> {
     // Trouver une Solution à travers un probleme donné;
@@ -13,4 +15,7 @@ public interface RepositorySolutions extends JpaRepository<Solutions, Long> {
     // Afficher une seule solution avec son problème et tous les commentaires;
     @Query(value = "SELECT * FROM solutions,problemes,users,commentaires WHERE solutions.id_problemes=:idSolutions AND problemes.id_users=users.id_users AND commentaires.id_solutions=solutions.id_solutions;",nativeQuery = true)
     Solutions solution(Long idSolutions);
+
+    @Query(value = "SELECT users.prenom,users.nom,solutions.description_solutions,problemes.id_problemes,solutions.estimation,solutions.methodologie_recherche,solutions.ressources,solutions.solutions,problemes.description_problemes,problemes.etat_problemes,problemes.technologies,problemes.titre_problemes,solutions.id_solutions FROM solutions,problemes,users,commentaires WHERE solutions.id_problemes=problemes.id_problemes AND commentaires.id_solutions=solutions.id_solutions;",nativeQuery = true)
+    List<Solutions> AfficherToutSurSolution();
 }
